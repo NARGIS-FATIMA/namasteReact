37479aa81af94c61737136aa76445a9798247ddd
Nargis Fatime <nargis.fatima160@gmail.com>
2023-12-26 20:09:11 +0530

    namasteReact

---
+++ App.js
@@ -0,0 +1,14 @@
+    const head = React.createElement("div",{id: "title" }
+    ,"nargis uth jao kam chor!!!");
+
+    const heading2 = React.createElement("h2",{
+        id:"title"
+    },"heading2");
+
+    const container=React.createElement ("div",{
+        id:"container"
+    }, [head,heading2])
+
+    console.log(head);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(container);
