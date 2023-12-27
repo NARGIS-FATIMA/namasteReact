57e850d874b9767b3b1b3f49efe35b83a90b69c4
Nargis Fatime <nargis.fatima160@gmail.com>
2023-12-27 19:41:33 +0530

    ignite our app

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+    import React from "react";
+    import ReactDOM from "react-dom/client";
     const head = React.createElement("div",{id: "title" }
     ,"nargis uth jao kam chor!!!");
 
     const heading2 = React.createElement("h2",{
         id:"title"
-    },"heading2");
+    },"heading2 for parcel");
 
     const container=React.createElement ("div",{
         id:"container"
